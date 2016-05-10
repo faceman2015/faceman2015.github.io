1cdd4f9799cfdb54bc6555b68cb69252511b1f8e
Tobi <tformanek@web.de>
2016-05-11 00:18:13 +0200

    Start und Endpunkt Button - aber uebernimmt nocht nicht die koordinaten

--- route/routing_map.js
+++ route/routing_map.js
@@ -1,46 +1,48 @@
-var layers = {
+var map = L.map('routingmap');
 
-		osmlayer: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
-		}),
+		}).addTo(map);
 
-		geolandbasemap: L.tileLayer("http://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
-			subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-			attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-		}),
-		osmlayer: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-			attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
-		}),
-
-		bmapoverlay: L.tileLayer("http://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
-			subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-			attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-		}),
-		bmapgrau: L.tileLayer("http://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
-			subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-			attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-		}),
-		bmaphidpi: L.tileLayer("http://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
-			subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-			attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-		}),
-		bmaporthofoto30cm: L.tileLayer("http://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
-			subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
-			attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
-		})
-    };
-
-    var routingkarte = L.map('routingmap', {
-        layers: [layers.osmlayer],
-    });
+    
 	
-	var routing = L.Routing.control({
+var routing = L.Routing.control({
   waypoints: [
     L.latLng(47.56, 12.12),
     L.latLng(47, 12.54)
   ],
   routeWhileDragging: true,
   //geocoder: L.Control.Geocoder.nominatim()
- 
-}).addTo(routingkarte);
-routingkarte.setView([47.56, 12.12]),9;
+}).addTo(map);
+
+//routingkarte.setView([47.56, 12.12]),9;
+
+function createButton(label, container) {
+    var btn = L.DomUtil.create('button', '', container);
+    btn.setAttribute('type', 'button');
+    btn.innerHTML = label;
+    return btn;
+}
+
+map.on('click', function(e) {
+    var container = L.DomUtil.create('div'),
+		startBtn = createButton('Startpunkt', container),
+		destBtn = createButton('Endpunkt', container);
+
+    L.popup()
+        .setContent(container)
+        .setLatLng(e.latlng)
+        .openOn(map);
+});
+
+	L.DomEvent.on(startBtn, 'click', function() {
+        control.spliceWaypoints(0, 1, e.latlng);
+        map.closePopup();
+    });
+	
+	L.DomEvent.on(destBtn, 'click', function() {
+        control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+        map.closePopup();
+    });
+	
+	

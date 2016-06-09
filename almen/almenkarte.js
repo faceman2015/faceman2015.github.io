028e28090385aa0e7ff43ddefa5bd673756a3981
Tobi <tformanek@web.de>
2016-06-09 11:11:23 +0200

    Fehlende Dateien vom vorgehenden commit hinzugefuegt

---
+++ almen/almenkarte.js
@@ -0,0 +1,38 @@
+window.onload = function() {
+
+    // Basis layer
+    var layers = {
+        osmlayer: L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap contributors</a>'
+        }),
+        geolandbasemap: L.tileLayer("http://{s}.wien.gv.at/basemap/geolandbasemap/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+            attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+        }),
+        bmapoverlay: L.tileLayer("http://{s}.wien.gv.at/basemap/bmapoverlay/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+            attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+        }),
+        bmapgrau: L.tileLayer("http://{s}.wien.gv.at/basemap/bmapgrau/normal/google3857/{z}/{y}/{x}.png", {
+            subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+            attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+        }),
+        bmaphidpi: L.tileLayer("http://{s}.wien.gv.at/basemap/bmaphidpi/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+            attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+        }),
+        bmaporthofoto30cm: L.tileLayer("http://{s}.wien.gv.at/basemap/bmaporthofoto30cm/normal/google3857/{z}/{y}/{x}.jpeg", {
+            subdomains: ['maps', 'maps1', 'maps2', 'maps3', 'maps4'],
+            attribution: 'Datenquelle: <a href="http://www.basemap.at/">basemap.at</a>'
+        })
+    };
+
+    // Karte erstellen
+    var map = L.map('almen', {
+        layers: [layers.osmlayer],
+        center: [47.00, 11.07],
+        zoom: 9
+    });
+
+    // Window.onload beenden:
+}
